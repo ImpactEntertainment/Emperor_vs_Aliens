c3ac14432acd3b7c220e75c6ef824ee9169e856b
Luis Gustavo <luisgustavodd@hotmail.com>
2013-03-03 12:42:09 -0300

    Arrumando a descricao da main.cpp

--- Development/EmperorVsAliens/source/main.cpp
+++ Development/EmperorVsAliens/source/main.cpp
@@ -1,11 +1,12 @@
 /**
- * \file tictactoe.cpp
- * \brief Módulo principal do jogo TicTacToe.
+ * \file main.cpp
+ * \brief Entrada do programa EmperorVsAliens.
  *
- * Módulo principal do jogo TicTacToe, responsável para inicialização
+ * Módulo principal do jogo EmperorVsAliens, responsável para inicialização
  * dos componentes e do laço principal.
  *
  * \author Edson Alves (edsonalves@unb.br)
+ * \author Luis Gustavo Souza Silva (luisgustavodd@hotmail.com)
  * \date 26/12/2012
  */
 #include <iostream>
