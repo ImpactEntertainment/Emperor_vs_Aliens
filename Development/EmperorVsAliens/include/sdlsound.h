eafd59d75c277cedbcccddd950664dbe7df1b960
leonn <leonn.paiva@gmail.com>
2013-03-07 15:57:38 -0300

    start sdlsound pattern

--- Development/EmperorVsAliens/include/sdlsound.h
+++ Development/EmperorVsAliens/include/sdlsound.h
@@ -15,19 +15,17 @@
 #include <SDL/SDL_mixer.h>
 
 #include "exception.h"
-#include "sound.h"
 
-namespace edge {
-
-    class SDL_Sound : public Sound {
+    class SDL_Sound {
     public:
-        SDL_Sound(Mix_Music *sound);
+        SDL_Sound * load(const string& soundPath) throw (Exception);
+        SDL_Sound(Mix_Chunk *sound);               
         ~SDL_Sound();
         static void play(SDL_Sound *sound) throw (Exception); 
        
-        Mix_Music *sound_stream;
+        Mix_Chunk *sound;
     };
-}
+
 
 #endif
 
